c20466942015a261655faea4054c7580ae138d4a
Graham Hornecker <grambh12@gmail.com>
2020-10-06 01:09:32 -0400

    Fantasy Game Inventory, v1.0 (Working + PEP 8)

--- practice_projects/fantasy_game_inventory/rpg_inventory.py
+++ practice_projects/fantasy_game_inventory/rpg_inventory.py
@@ -1,1 +1,15 @@
-inventory = {}
+inventory = {'rope': 1, 'torch': 6, 'gold coin': 42, 'dagger': 1, 'arrow': 12}
+
+
+def display_inventory(inv):
+    """Displays the inventory dictionary passed."""
+    print("Inventory:")
+    total_items = 0
+    for item in inv.keys():
+        total_items += int(inv[item])
+        print(inv[item], item)
+    print("Total number of items: " + str(total_items))
+
+
+if __name__ == "__main__":
+    display_inventory(inventory)
