5e33a935e8d99f6f19e0144b841d01665d8cc628
Graham Hornecker <grambh12@gmail.com>
2020-10-07 20:27:14 -0400

    MCLIP Project Step 2

--- practice_projects/multi_clipboard_automatic_messages/mclip.py
+++ practice_projects/multi_clipboard_automatic_messages/mclip.py
@@ -1,8 +1,17 @@
 #! python3
 # mclip.py - A multi-clipboard program.
 
+import sys
+
+
 TEXT = {
     'agree': """Yes, I agree. That sounds fine to me.""",
     'busy': """Sorry, can we do this later this week or next week?""",
     'upsell': """Would you consider making this a monthly donation?"""
 }
+
+if len(sys.argv) < 2:
+    print("Usage: python mclip.py [keyphrase] - copy phrase text")
+    sys.exit()
+
+keyphrase = sys.argv[1]
