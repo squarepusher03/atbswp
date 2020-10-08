14f83a96129a7b98e824019afa2243a16d778414
Graham Hornecker <grambh12@gmail.com>
2020-10-08 01:13:09 -0400

    Table Printer Complete!

---
+++ practice_projects/table_printer/table_printer.py
@@ -0,0 +1,14 @@
+# The table that's going to printed
+tableData = [['apples', 'oranges', 'cherries', 'banana'],
+             ['Alice', 'Bob', 'Carol', 'David'],
+             ['dogs', 'cats', 'mouse', 'goose']]
+
+# Finds the max amount of chars in each column.
+colWidth = []
+for i in range(len(tableData)):
+    colWidth.append(len(max(tableData[i], key=len)))
+
+for i in range(len(tableData[0])):
+    for j in range(len(tableData)):
+        print(tableData[j][i].rjust(colWidth[j])+" ", end='')
+    print()
