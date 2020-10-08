cf56301b1b9932e06919366216e7e29f16150a79
Graham Hornecker <grambh12@gmail.com>
2020-10-07 20:51:22 -0400

    MCLIP Project Step 3

--- practice_projects/multi_clipboard_automatic_messages/mclip.py
+++ practice_projects/multi_clipboard_automatic_messages/mclip.py
@@ -1,6 +1,7 @@
 #! python3
 # mclip.py - A multi-clipboard program.
 
+import pyperclip
 import sys
 
 
@@ -14,4 +15,10 @@ if len(sys.argv) < 2:
     print("Usage: python mclip.py [keyphrase] - copy phrase text")
     sys.exit()
 
-keyphrase = sys.argv[1]
+keyphrase = sys.argv[1]  # first command line arg is the keyphrase
+
+if keyphrase in TEXT:
+    pyperclip.copy(TEXT[keyphrase])
+    print('Text for ' + keyphrase + ' copied to clipboard.')
+else:
+    print('There is no text for ' + keyphrase)
