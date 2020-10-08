9202bb5b10368296423e5ddb9afc29ffbe96e5fe
Graham H <48061989+squarepusher03@users.noreply.github.com>
2020-10-08 01:16:25 -0400

    Update table_printer.py

--- practice_projects/table_printer/table_printer.py
+++ practice_projects/table_printer/table_printer.py
@@ -8,6 +8,7 @@ colWidth = []
 for i in range(len(tableData)):
     colWidth.append(len(max(tableData[i], key=len)))
 
+# Prints out each column.
 for i in range(len(tableData[0])):
     for j in range(len(tableData)):
         print(tableData[j][i].rjust(colWidth[j])+" ", end='')
