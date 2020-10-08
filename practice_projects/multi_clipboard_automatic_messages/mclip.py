78c8aace3e679b4b43d986d065223c205a33fc5e
Graham Hornecker <grambh12@gmail.com>
2020-10-07 20:16:17 -0400

    MCLIP Project Step 1

---
+++ practice_projects/multi_clipboard_automatic_messages/mclip.py
@@ -0,0 +1,8 @@
+#! python3
+# mclip.py - A multi-clipboard program.
+
+TEXT = {
+    'agree': """Yes, I agree. That sounds fine to me.""",
+    'busy': """Sorry, can we do this later this week or next week?""",
+    'upsell': """Would you consider making this a monthly donation?"""
+}
